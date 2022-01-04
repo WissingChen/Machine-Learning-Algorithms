596132dd6da66fc354d72ee9736def2d039ed3cb
Wissing Chen <31480397+WissingChen@users.noreply.github.com>
2022-01-04 14:47:56 +0800

    add lsqr solver for ridge

--- main.py
+++ main.py
@@ -5,18 +5,19 @@
 # @file    : main.py
 # @time    : 2021/7/21 13:06
 
-# from sklearn.datasets import load_breast_cancer  # 2 classes
+from sklearn.datasets import load_breast_cancer  # 2 classes
 # from sklearn.datasets import load_iris  # 3 classes
 # from sklearn.datasets import load_digits  # image   10 classes
 from sklearn.datasets import load_diabetes  # regression
+from utils.metric.regression import mse
+from utils.metric.binary import accuracy
 
 import numpy as np
-from utils.metric.regression import mse
 from model.linear import Ridge as ME
 from sklearn.linear_model import Ridge as SK
 
 if __name__ == '__main__':
-    x, y = load_diabetes(return_X_y=True)
+    x, y = load_breast_cancer(return_X_y=True)
     m, n = x.shape
     data = np.concatenate([x, y.reshape([m, -1])], axis=1)
     np.random.shuffle(data)
@@ -24,7 +25,7 @@ if __name__ == '__main__':
     train = data[_m:]
     test = data[:_m]
     # model
-    model = ME()
+    model = ME(solver='lsqr')
     model_gt = SK()
     model.fit(train[:, :n], train[:, n:])
     model_gt.fit(train[:, :n], train[:, n:])
@@ -32,5 +33,5 @@ if __name__ == '__main__':
     pre = model.predict(test[:, :n])
     pre_gt = model_gt.predict(test[:, :n])
     print(model.score())
-    print("my model: ", mse(pre, test[:, n:]),
-          "\nsci-kit model: ", mse(pre_gt, test[:, n:]))
+    print("my model: ", accuracy(pre, test[:, n:]),
+          "\nsci-kit model: ", accuracy(pre_gt, test[:, n:]))
--- model/linear/_ridge.py
+++ model/linear/_ridge.py
@@ -5,21 +5,26 @@
 # @file    : Ridge.py
 # @time    : 1/12/2021 下午6:54
 import numpy as np
+from scipy.sparse import linalg as sp_linalg
 from ..base import Base
 from utils.metric.regression import mse
 
 
 class Ridge(Base):
-    def __init__(self, alpha=.1):
+    def __init__(self, alpha=.1, solver='none'):
         Base.__init__(self)
         self.w = None
         self.alpha = alpha
+        self.solver = solver
 
     def fit(self, x, y):
         m, n = x.shape
         y = y.reshape([-1, 1])
         x = np.concatenate([np.ones([m, 1]), x], axis=1)
-        self.w = np.linalg.inv(np.dot(x.T, x) + self.alpha * np.eye(n+1)).dot(x.T).dot(y)
+        if self.solver == 'lsqr':
+            self.w = _lsqr(x, y, self.alpha)
+        elif self.solver == 'none':
+            self.w = np.linalg.inv(np.dot(x.T, x) + self.alpha * np.eye(n+1)).dot(x.T).dot(y)
         self._score = mse(np.dot(x, self.w), y)
 
     def predict(self, x):
@@ -29,3 +34,29 @@ class Ridge(Base):
 
     def score(self):
         return self._score
+
+
+def _lsqr(x, y, alpha):
+    """
+    References
+    ----------
+    .. [1] C. C. Paige and M. A. Saunders (1982a).
+           "LSQR: An algorithm for sparse linear equations and
+           sparse least squares", ACM TOMS 8(1), 43-71.
+    .. [2] C. C. Paige and M. A. Saunders (1982b).
+           "Algorithm 583.  LSQR: Sparse linear equations and least
+           squares problems", ACM TOMS 8(2), 195-209.
+    .. [3] M. A. Saunders (1995).  "Solution of sparse rectangular
+           systems using LSQR and CRAIG", BIT 35, 588-604.
+    """
+    n, n = x.shape
+    w = np.zeros([n, y.shape[1]])
+
+    sqrt_alpha = np.sqrt(alpha)
+
+    for i in range(y.shape[1]):
+        y_column = y[:, i]
+        info = sp_linalg.lsqr(x, y_column, damp=sqrt_alpha)
+        w[:, i] = info[0]
+
+    return w
